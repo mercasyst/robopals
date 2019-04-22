1e7e538fb669ebb7f79d629d0e4a7b5585a59bb3
Dmitriy Gontsov <DmGontsov@outlook.com>
2019-04-22 19:23:29 +0500

    Updates

---
+++ precache-manifest.32d7e890fddcf10bcbfee2869f94e2af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "49af419ce945a0dc2e03",
+    "url": "/robopals/static/css/main.07a542a3.chunk.css"
+  },
+  {
+    "revision": "49af419ce945a0dc2e03",
+    "url": "/robopals/static/js/main.9a6185e1.chunk.js"
+  },
+  {
+    "revision": "85ca4ef05e834621cc56",
+    "url": "/robopals/static/js/runtime~main.1d0bf8ea.js"
+  },
+  {
+    "revision": "a8518ee9757610a0e8d0",
+    "url": "/robopals/static/css/2.48f56bbb.chunk.css"
+  },
+  {
+    "revision": "a8518ee9757610a0e8d0",
+    "url": "/robopals/static/js/2.2c4d7ad8.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robopals/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdd271fa958f5244e7e660f987962115",
+    "url": "/robopals/index.html"
+  }
+];
